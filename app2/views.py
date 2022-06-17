61a6552e9593bac09f552c54dc6fdb2b10eaf61c
Rohit Kumar Lodhi <rohit.kumar@skypointcloud.com>
2022-06-17 18:14:22 +0530

    first commit

---
+++ app2/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here
+
+def virat(request):
+    return HttpResponse('Virat is the best batsman in the world')
+
+   
